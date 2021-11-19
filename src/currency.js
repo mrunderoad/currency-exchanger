80aa2ee4c4459c84083b9e6b35ffbebea2b99383
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-19 10:40:33 -0800

    Add first steps to business logic. WIP

--- src/currency.js
+++ src/currency.js
@@ -1,15 +1,15 @@
-class CurrencyExchange {
+export default class CurrencyExchange {
   static getDino() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${'API_KEY'}/latest/USD`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import CurrencyExchange from './currency.js';
+import CurrencyExchange from './currency.js';
 
-// function showEuro(money) 
+// function clearFields() {
+//   $('.amount').val("");
+// } 
+
+function getExchange(response) {
+  for(let i=0;i<response.exchange.length; i++) {
+    let exchange = response.exchange[i].currency;
+  
+  $(".showResult").append("<li>" + exchange + "</li>");
+  }
+}
  
 
 
 
 
 
-// $(document).ready(function () {
-//   $
-
-// })
+$(document).ready(function () {
+  let promise = CurrencyExchange.getExchange();
+  promise.then(function () {
+    console.log(getExchange);
+  });
+  $('#formOne').submit(function (event) {
+    event.preventDefault();
+    const input = $('.amount').val();
+    $('.showResult').text(input);
+  });
+});

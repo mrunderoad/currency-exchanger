1d282f038fc190c5514a2f0708f9f67beced7c94
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-19 13:01:07 -0800

    refactoring js code

--- src/currency.js
+++ src/currency.js
@@ -14,4 +14,23 @@ export default class CurrencyExchange {
       request.send();
     });
   }
+  converter(body, number) {
+  const eur = parseFloat(body[0].cost)
+  const gbp = parseFloat(body[0].cost)
+  const cad = parseFloat(body[0].cost)
+  const aud = parseFloat(body[0].cost)
+  const jpy = parseFloat(body[0].cost)
+  let convertedArray = [];
+
+  if (number !== 0) {
+    convertedArray.push(`USD to EUR : ${number/eur}`);
+    convertedArray.push(`USD to GBP : ${number/gbp}`);
+    convertedArray.push(`USD to CAD : ${number/cad}`);
+    convertedArray.push(`USD to AUD : ${number/aud}`);
+    convertedArray.push(`USD to JPY : ${number/jpy}`);
+  } else {
+    return NaN;
+  }
+  return convertedArray;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,29 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './currency.js';
 
-// function clearFields() {
-//   $('.amount').val("");
-// } 
-
-function getExchange(response) {
-  for(let i=0;i<response.exchange.length; i++) {
-    let exchange = response.exchange[i].currency;
-    let uinput = userInput;  
-  
-  if (userInput === ".eur") {
-    $('#eur').text(uinput, exchange);
-    } else if (userInput === ".gbp") {
-    $('#gbp').text(uinput, exchange);
-
-    } 
-  }
-}
- 
-
-
-
-
-
 $(document).ready(function () {
   let promise = CurrencyExchange.getExchange();
   promise.then(function () {
@@ -46,7 +23,7 @@ $(document).ready(function () {
   
 
 
-  // function showError (error) {
-  //   $('.showErrors').text(`There was an error processing your request: ${error}`);
-  // }
-});
+//   // function showError (error) {
+//   //   $('.showErrors').text(`There was an error processing your request: ${error}`);
+//   // }
+ });
